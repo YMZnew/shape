c1377a0a8e51a7b537bf7aa9fa98acf9f434b6a4
YMZnew <53408011+YMZnew@users.noreply.github.com>
2020-12-14 03:38:29 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,8 @@ function startWebcam() {
 		navigator.mediaDevices
 			.getUserMedia({ 
 video: { 
-width: { min: 320, max: 480 },
-		height: { min: 240, max: 360 }
+width: { min: 240, max:240  },
+		height: { min: 320, max: 320 }
 }
  })
 			.then(function (stream) {
