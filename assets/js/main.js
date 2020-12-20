778f0736ade88121064d83e05b3f8b17451f11a5
YMZnew <53408011+YMZnew@users.noreply.github.com>
2020-12-21 02:49:45 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -478,7 +478,7 @@ function detectShapeWithColor(img) {
 			//const shapeColor = detectColor(src, contours, i, cnt);
 			var labelText = contourArea+' ';
 try{
-labelText = contourArea+' ' + hierarchy.size();
+labelText = contourArea+' ' + hierarchy[0][i][3];
 }catch(e){
 alert(e);
 }
