f732b074f294c80bdcc8de330ad341cd808769ec
YMZnew <53408011+YMZnew@users.noreply.github.com>
2020-12-25 02:40:30 +0330

    Update tri.js

--- assets/js/tri.js
+++ assets/js/tri.js
@@ -425,7 +425,7 @@ function detectShapeWithColor(img) {
 		/* exclude smaller contours for reducing noise */
 
 /* default 1000 */
-		if (contourArea > 1000 ) {
+		if (contourArea > 100 ) {
 			// console.log(contourArea);
 
 			/* get the shape of current cnt */
