31a872993605122d1ede65f32118850f20811e91
YMZnew <53408011+YMZnew@users.noreply.github.com>
2020-12-25 03:44:14 +0330

    Update tri.js

--- assets/js/tri.js
+++ assets/js/tri.js
@@ -460,9 +460,9 @@ if(parentShape == 'Triangle'){
 	//document.getElementById("status").innerHTML = "parent is triangle";
 			/* get color of current cnt */
 			//const shapeColor = detectColor(src, contours, i, cnt);
-				var child  = hierarchy.intPtr(0,i)[2];
-				var parent = hierarchy.intPtr(0,i)[3];
- 			var labelText = contourArea+' child = ' +   child + ' parent = '+ parent;
+				//var child  = hierarchy.intPtr(0,i)[2];
+				//var parent = hierarchy.intPtr(0,i)[3];
+ 			var labelText = contourArea /*+' child = ' +   child + ' parent = '+ parent*/;
 
 	//document.getElementById("status").innerHTML = "label set";
 			// /* generates random color */
